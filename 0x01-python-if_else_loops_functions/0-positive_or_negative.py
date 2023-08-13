a3f81f6123d305b76eced06892b1f8234030c50b
Eric Acquah <ericacquah1720@gmail.com>
2023-08-13 15:13:09 +0000

    Positive or Negative program

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+#This program checks if the number is positive or negative
+
+if number < 0:
+    print(f"{number:d} is negative")
+elif number > 0:
+    print(f"{number:d} is positive")
+else:
+    print(f"{number:d} is zero")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This repo explores control flow and also functions of the python language and how they compare to C
