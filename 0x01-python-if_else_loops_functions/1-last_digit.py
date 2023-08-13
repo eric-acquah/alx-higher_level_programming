3aad4ea38588bc3fb4ba69993ea144c0f0bc002a
Eric Acquah <ericacquah1720@gmail.com>
2023-08-13 18:08:17 +0000

    Fixed typError

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,16 +11,17 @@ If number is < 6 and not 0: print is less than or 6 and not zero
 number = random.randint(-10000, 10000)
 last_msg = "Last digit of"
 less_than_6 = "less than 6 and not 0"
-last_digit = str(number)
-last_digit = int(last_digit[-1])
 
-# in case number is negative, convert last_digit to negative
 if number < 0:
+    neg_num = -number
+    last_digit = neg_num % 10
     last_digit = last_digit * -1
+else:
+    last_digit = number % 10
 
 if last_digit == 0:
-    print(f"{last_msg} {number:d} is {last_digit:d} and is 0")
+    print(f"{last_msg:s} {number:d} is {last_digit:d} and is 0")
 elif last_digit < 6 and not 0:
-    print(f"{last_msg} {number:d} is {last_digit:d} and is {less_than_6}")
+    print(f"{last_msg:s} {number:d} is {last_digit:d} and is {less_than_6:s}")
 elif last_digit > 5:
-    print(f"{last_msg} {number:d} is {last_digit:d} and is greater than 5")
+    print(f"{last_msg:s} {number:d} is {last_digit:d} and is greater than 5")
