da097894f3124f6a9bb0f79ea28cab98196da2f9
Eric Acquah <ericacquah1720@gmail.com>
2023-08-13 15:15:01 +0000

    Conforming to PEP8 block comment conventions

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 
-#This program checks if the number is positive or negative
+# This program checks if the number is positive or negative
 
 if number < 0:
     print(f"{number:d} is negative")
