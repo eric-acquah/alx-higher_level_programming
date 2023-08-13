d85cb04e4c78d027154680952176b3c300015118
Eric Acquah <ericacquah1720@gmail.com>
2023-08-13 17:05:55 +0000

    more on control flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -21,6 +21,6 @@ if number < 0:
 if last_digit == 0:
     print(f"{last_msg:s} {number:d} is {last_digit:d} and is 0")
 elif last_digit < 6 and not 0:
-    print(f"{last_msg:s} {number:d} is {last_digit:d} and is {less_than_6}")
+    print(f"{last_msg:s} {number:d} is {last_digit:d} and is {less_than_6:s}")
 elif last_digit > 5:
     print(f"{last_msg:s} {number:d} is {last_digit:d} and is greater than 5")
