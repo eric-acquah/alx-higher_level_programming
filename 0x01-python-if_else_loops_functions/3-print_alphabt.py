ab82bb92ad9a25cd732c5421d64817611cb340b2
Eric Acquah <ericacquah1720@gmail.com>
2023-08-14 14:32:05 +0000

    removed continue statement and used an inverse of condition

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 '''
-Print the alpahbet in lowercase letters except for "e" and "q"
+Print the alphabet in lowercase letters except for "e" and "q"
 '''
 
 for i in range(ord('a'), (ord('z') + 1)):
-    # if i == ord('e') or i == ord('q'):
-        # continue
-    print("{:c}".format(i), end="")
+    if i != ord('e') or i != ord('q'):
+        print("{:c}".format(i), end="")
