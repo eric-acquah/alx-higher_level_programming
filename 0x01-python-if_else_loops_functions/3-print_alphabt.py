63b0c914fa5c281551e1ee4559f282549f9b116d
Eric Acquah <ericacquah1720@gmail.com>
2023-08-14 13:50:47 +0000

    Test run2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -5,6 +5,6 @@ Print the alpahbet in lowercase letters except for "e" and "q"
 '''
 
 for i in range(ord('a'), (ord('z') + 1)):
-    if i == ord('e') or i == ord('q'):
-        continue
+    # if i == ord('e') or i == ord('q'):
+        # continue
     print("{:c}".format(i), end="")
