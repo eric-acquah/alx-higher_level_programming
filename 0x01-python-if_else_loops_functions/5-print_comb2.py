3b1eab15dac7767b6b241a32ba931de9f105ea29
Eric Acquah <ericacquah1720@gmail.com>
2023-08-14 20:13:14 +0000

    Achieved the same result we fewer print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -6,25 +6,17 @@ Print numbers from 00 to 99 with "," and " " as delimeter
 
 f1 = 0
 f2 = 0
+delim = ", "
 
 for i in range(99):
 
     if i == 98:
-        if f2 > 8:
-            f1 += 1
-        print("{:d}".format(f1), end="")
-        if f2 > 8:
-            f2 = 0
-        else:
-            f2 += 1
-        print("{:d}".format(f2), end="")
+        delim = "\n"
+    if f2 > 8:
+        f1 += 1
+    print("{:d}".format(f1), end="")
+    if f2 > 8:
+        f2 = 0
     else:
-        if f2 > 8:
-            f1 += 1
-        print("{:d}".format(f1), end="")
-        if f2 > 8:
-            f2 = 0
-        else:
-            f2 += 1
-        print("{:d}".format(f2), end=", ")
-print('')
+        f2 += 1
+    print("{:d}{}".format(f2, delim), end="")
