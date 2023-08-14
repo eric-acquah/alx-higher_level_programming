e03ba1a2cdb409320b4d7cb591a381fc0dd487db
Eric Acquah <ericacquah1720@gmail.com>
2023-08-14 18:25:12 +0000

    Print form 00 to 99 using a single loop

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+Print numbers from 00 to 99 with "," and " " as delimeter
+'''
+
+f1 = 0
+f2 = 0
+
+for i in range(99):
+
+    if i == 98:
+        if f2 > 8:
+            f1 += 1
+        print("{:d}".format(f1), end="")
+        if f2 > 8:
+            f2 = 0
+        else:
+            f2 += 1
+        print("{:d}".format(f2), end="")
+    else:
+        if f2 > 8:
+            f1 += 1
+        print("{:d}".format(f1), end="")
+        if f2 > 8:
+            f2 = 0
+        else:
+            f2 += 1
+        print("{:d}".format(f2), end=", ")
+print('')
