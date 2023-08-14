c3b3e0524442147e89277ea74202e5227ab4c7f2
Eric Acquah <ericacquah1720@gmail.com>
2023-08-14 22:49:00 +0000

    Test run5

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,6 +4,6 @@
 Print the alphabet in lowercase letters except for "e" and "q"
 '''
 
-for i in range(ord('a'), (ord('z') + 1)):
+for i in range(97, 123):
     if chr(i) not in "qe":
         print("{:c}".format(i), end="")
