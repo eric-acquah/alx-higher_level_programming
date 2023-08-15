902d11d54581afb17e72d7dde3116002d13a503a
Eric Acquah <ericacquah1720@gmail.com>
2023-08-15 15:00:56 +0000

    Excluding newline

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -16,8 +16,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end=" " if i != 100 else " \n")
+            print("Buzz", end=" ")
         else:
             print("{:d}".format(i), end=" ")
-
-fizzbuzz()
