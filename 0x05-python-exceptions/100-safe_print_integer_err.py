0d53030cde59865c49c03af358a028cee8954762
Eric Acquah <ericacquah1720@gmail.com>
2023-08-29 02:24:03 +0000

    print error msg to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+# Print the given value in integer format
+# Return: true if successful else false
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
