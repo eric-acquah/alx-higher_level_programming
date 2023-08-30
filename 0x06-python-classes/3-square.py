456bfe621197a30fdc7fe1350044042258f17b8d
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 02:14:14 +0000

    Define a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class with private attributes
+"""
+
+
+class Square:
+    """
+    This class defines a simple class with a private object attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        This method initializes objects with the size varaible
+
+        Args:
+            size: constant size of square
+        """
+
+        self.__size = size
+
+        # size must be an integer
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+
+        # size must be 0 or geater
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Calculates for the area of a square
+        """
+
+        return self.__size * self.__size
