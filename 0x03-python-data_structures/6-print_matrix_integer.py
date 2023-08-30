1b3f98fe78155ac6ee5015942d39b2755f6661f8
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 10:54:47 +0000

    print a list matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for ex_arr in matrix:
+        for in_arr in ex_arr:
+            print("{:d}".format(in_arr), end="\n" if in_arr == ex_arr[-1]
+                  else " ")
