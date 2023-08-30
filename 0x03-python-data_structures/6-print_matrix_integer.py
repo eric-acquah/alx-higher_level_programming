9ab85dd5553cbdd3c3f053cdc841c1662e21d571
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 11:11:29 +0000

    empty matrix eadge case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,9 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for ex_arr in matrix:
-        for in_arr in ex_arr:
-            print("{:d}".format(in_arr), end="\n" if in_arr == ex_arr[-1]
-                  else " ")
+
+    if not len(matrix) == 1:
+        for ex_arr in matrix:
+            for in_arr in ex_arr:
+                print("{:d}".format(in_arr), end="\n" if in_arr == ex_arr[-1]
+                      else " ")
