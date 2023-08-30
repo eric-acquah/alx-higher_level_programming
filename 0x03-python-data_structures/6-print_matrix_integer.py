6569875c2963f2c3f1ad56ea7a9d1d7e76cf4fe5
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 11:20:49 +0000

    print an empty line if matrix is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    if not len(matrix) == 1:
-        for ex_arr in matrix:
-            for in_arr in ex_arr:
-                print("{:d}".format(in_arr), end="\n" if in_arr == ex_arr[-1]
-                      else " ")
+    if not matrix:
+        print('')
+        return
+    for ex_arr in matrix:
+        for in_arr in ex_arr:
+            print("{:d}".format(in_arr), end="\n" if in_arr == ex_arr[-1]
+                  else " ")
