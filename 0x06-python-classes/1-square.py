2b2dfb8d4f1051347ae654cffcf6367761aec4af
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 00:34:29 +0000

    create a class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class with private attributes
+"""
+
+class Square:
+    """
+    This class defines a simple class with a private object attribute
+    """
+
+    def __init__(self, size=None):
+        """
+        This method initializes objects with the size varaible
+
+        Args:
+            size: constant size of square
+        """
+
+        self.__size = size
