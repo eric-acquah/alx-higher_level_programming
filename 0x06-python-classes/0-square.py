3d8b58a2d8d3d6b178eaedee564a20c1819ff4c0
Eric Acquah <ericacquah1720@gmail.com>
2023-08-30 00:02:47 +0000

    The simplest class you could ever get in python

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module simply creates an empty class
+"""
+
+
+class Square:
+    """
+    This class does nothing, it only defines `Square` as a class
+    """
+    pass
