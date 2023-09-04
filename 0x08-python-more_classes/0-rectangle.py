77e1a9a1e1bb0df2b6da04d6a3f123df0d802663
Eric Acquah <ericacquah1720@gmail.com>
2023-09-04 17:33:05 +0000

    creating an empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Create an empty class
+"""
+
+
+class Rectangle:
+    """
+    creating an empty class
+    """
+
+    pass # do nothing
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+More implementations of classes and objects
