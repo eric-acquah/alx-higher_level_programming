3edb44a37b8beea071da034cca69acb4fa82d361
Eric Acquah <ericacquah1720@gmail.com>
2023-09-13 16:47:20 +0000

    load and save into json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A script to add commandline args to a list
+and save it into a file in json format
+
+"""
+
+import json
+import sys
+json_save = __import__("5-save_to_json_file").save_to_json_file
+json_load = __import__("6-load_from_json_file").load_from_json_file
+
+argv_list = []
+
+for args in range(1, len(sys.argv)):
+    argv_list.append(sys.argv[args])
+
+json_save(argv_list, "add_item.json")
+
+json_load("add_item.json")
