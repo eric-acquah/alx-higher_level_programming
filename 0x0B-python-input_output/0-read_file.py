f3d6f2f3d2ce25c8ef459f3ae8e591d440d92dd3
Eric Acquah <ericacquah1720@gmail.com>
2023-09-13 11:42:51 +0000

    read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/python3
+
+"""
+File handeling: open file
+
+"""
+
+
+def read_file(filename=""):
+    """
+    read the content within a file and print it to stdout
+
+    Args:
+        filename (str): the file to open and read from
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
