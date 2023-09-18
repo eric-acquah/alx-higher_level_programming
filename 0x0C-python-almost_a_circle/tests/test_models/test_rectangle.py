4600624df2bb337a48ae4732e2726892b201b422
Eric Acquah <ericacquah1720@gmail.com>
2023-09-18 15:09:34 +0000

    define method to calculate area

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,7 +11,9 @@ class RectangleTest(unittest.TestCase):
     """Test all scenarios"""
 
     def test_instanceId(self):
-        new_id = Rectangle(10, 2)
+        """check instance ids"""
+
+        new_id = Rectangle(10, 2, 4, 7, 4)
         self.assertEqual(new_id.id, 4)
 
         another_id = Rectangle(2, 10)
@@ -21,14 +23,17 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(id_again.id, 12)
 
     def test_instanceAttribute(self):
+        """Check the other attributes"""
+
         new = Rectangle(10, 2)
-        self.assertEqual(new.id, 3)
         self.assertEqual(new.width, 10)
         self.assertEqual(new.height, 2)
         self.assertEqual(new.x, 0)
         self.assertEqual(new.y, 0)
 
     def test_instanceAttributeTypeException(self):
+        """Type exceptions with instance attributes"""
+
         with self.assertRaises(TypeError):
             Rectangle("10", 2, 0, 0, 12)
 
@@ -45,6 +50,8 @@ class RectangleTest(unittest.TestCase):
             Rectangle(10, 2, 0, 0, "12")
 
     def test_instanceAttributeValueException(self):
+        """Exceptions at initailization level"""
+
         with self.assertRaises(ValueError):
             Rectangle(0, 2, 0, 0, 12)
 
@@ -58,6 +65,8 @@ class RectangleTest(unittest.TestCase):
             Rectangle(10, 2, 0, -7, 12)
 
     def test_instance_getters_setters(self):
+        """check for instance getter/setter accuracy"""
+
         r1 = Rectangle(10, 2, 4, 6)
         r1.width += 1
         r1.height *= 2
@@ -69,6 +78,8 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(r1.y, 8)
 
     def test_getter_setter_TypeException(self):
+        """ Check getter setter type exceptions """
+
         r1 = Rectangle(10, 2, 4, 6)
 
         with self.assertRaises(TypeError):
@@ -84,6 +95,8 @@ class RectangleTest(unittest.TestCase):
             r1.y += "1"
 
     def test_getter_setter_ValueException(self):
+        """ Check getter setter value exceptions """
+
         r1 = Rectangle(10, 2, 4, 6)
 
         with self.assertRaises(ValueError):
@@ -97,3 +110,29 @@ class RectangleTest(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             r1.y = -12
+
+    def test_area(self):
+        """Test area method"""
+
+        value = Rectangle(10, 2)
+        self.assertEqual(value.area(), 20)
+
+        value.width = 2
+        value.height = 4
+        self.assertEqual(value.area(), 8)
+
+        value.width = 1
+        value.height = 1
+        self.assertEqual(value.area(), 1)
+
+        with self.assertRaises(ValueError):
+            value.width = -1
+
+        with self.assertRaises(ValueError):
+            value.height = 0
+
+        with self.assertRaises(TypeError):
+            value.width = "-1"
+
+        with self.assertRaises(TypeError):
+            value.height = "11"
