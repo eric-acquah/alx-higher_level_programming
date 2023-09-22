c9427b0a688158b1dcf12c59d29f469810bfce61
Eric Acquah <ericacquah1720@gmail.com>
2023-09-22 02:15:19 +0000

    edge cases

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,11 +53,12 @@ class Base:
         """
 
         with open(f"{list_objs[0].__class__.__name__}.json", 'w') as f:
-            if list_objs is None:
-                f.write("[]")
-                return
 
             obj_list = []  # list to store a dictionary of instance attributes
+
+            if list_objs is None:
+                f.write(cls.to_json_string(obj_list))
+                return
 
             for li in list_objs:
                 obj_list.append(li.to_dictionary())
