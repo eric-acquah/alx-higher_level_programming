c6f6d11b3e1f652063ea6506784e1d449c2caa97
Eric Acquah <ericacquah1720@gmail.com>
2023-11-15 23:38:03 +0000

    Find second largest value in a sequence

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+/*
+Search for the second biggest number
+*/
+
+const argv = process.argv;
+
+function secondBiggest (list) {
+  let prevMax = Math.floor(Number(list[3]));
+  let currentMax = Math.floor(Number(list[2]));
+
+  for (let i = 2; i < list.length; i++) {
+    // convert current value to integer
+    let val = Math.floor(Number(list[i]));
+
+    if (currentMax < val) {
+      // Give the previous largest value to prev_max
+      prevMax = currentMax;
+      // store the currently largest
+      currentMax = val;
+    } else if ((prevMax < val) && (val !== currentMax)) {
+      // (prevMax !== undefined) && (prevMax < val)
+      // if the stored largest value is still larger yet currently read value is
+      // larger than the second largest, then store the given value
+      // as the second largest
+      prevMax = val;
+    }
+    console.log('CM => ' + currentMax);
+    console.log('pM => ' + prevMax);
+  }
+
+  return prevMax;
+}
+
+if (argv.length <= 3) {
+  console.log('0');
+} else {
+  console.log(secondBiggest(argv));
+}
