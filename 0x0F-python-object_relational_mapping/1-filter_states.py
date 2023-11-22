85b8087767b208653f96667667b508f46a306ff1
Eric Acquah <ericacquah1720@gmail.com>
2023-11-22 12:57:17 +0000

    Filter results

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Execute select query on a given database
+
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    user_name = sys.argv[1]
+    key = sys.argv[2]
+    user_db = sys.argv[3]
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=user_name,
+        port=3306,
+        passwd=key,
+        db=user_db
+    )
+
+    cursor = connection.cursor()
+
+    query = 'SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id'
+
+    cursor.execute(query)  # query the database
+    output = cursor.fetchall()
+
+    # Print output
+    for row in output:
+        print(row)
+
+    # cleanup action
+    cursor.close()
+    connection.close()
