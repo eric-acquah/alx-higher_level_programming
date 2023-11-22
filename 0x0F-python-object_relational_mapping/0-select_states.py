a50a94c42734664405ff1b66008e51e0f2e7ce50
Eric Acquah <ericacquah1720@gmail.com>
2023-11-22 12:13:39 +0000

    Checking issue

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Execute select query on a given database
+
+"""
+
+import MySQLdb
+import sys
+
+
+user_name = sys.argv[1]
+key = sys.argv[2]
+user_db = sys.argv[3]
+
+connection = MySQLdb.connect(
+    host='localhost',
+    user=user_name,
+    port=3306,
+    passwd=key,
+    db=user_db
+)
+
+cursor = connection.cursor()
+
+query = 'SELECT * FROM states ORDER BY states.id'
+
+cursor.execute(query)  # query the database
+output = cursor.fetchall()
+
+# Print output
+for row in output:
+    print(row)
