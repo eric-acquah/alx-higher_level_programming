5e6c85505c6734f14469b121b87228010c2c9e75
Eric Acquah <ericacquah1720@gmail.com>
2023-11-22 12:22:06 +0000

    Prevented the module from working when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,25 +9,30 @@ import MySQLdb
 import sys
 
 
-user_name = sys.argv[1]
-key = sys.argv[2]
-user_db = sys.argv[3]
+if __name__ == '__main__':
+    user_name = sys.argv[1]
+    key = sys.argv[2]
+    user_db = sys.argv[3]
 
-connection = MySQLdb.connect(
-    host='localhost',
-    user=user_name,
-    port=3306,
-    passwd=key,
-    db=user_db
-)
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=user_name,
+        port=3306,
+        passwd=key,
+        db=user_db
+    )
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
-query = 'SELECT * FROM states ORDER BY states.id'
+    query = 'SELECT * FROM states ORDER BY states.id'
 
-cursor.execute(query)  # query the database
-output = cursor.fetchall()
+    cursor.execute(query)  # query the database
+    output = cursor.fetchall()
 
-# Print output
-for row in output:
-    print(row)
+    # Print output
+    for row in output:
+        print(row)
+
+    # cleanup action
+    cursor.close()
+    connection.close()
