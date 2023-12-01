b4ee90ef5ce224f651bb29461e8c5edafcfd40de
Eric Acquah <ericacquah1720@gmail.com>
2023-12-01 00:15:22 +0000

    Conditional reponse body display

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Display body only if reponse status is 200
+view=$(curl -s -w %"{http_code}" -o response.txt -X GET http://"$1"); if [[ $view == "200" ]]; then cat response.txt; echo ""; fi
